0a4cb39da63c74d762654c2ecfc0d9718d5a9e41
Ivan Tsai <oscar60310@gmail.com>
2019-10-30 17:34:32 +0800

    code splitting

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,37 +5,62 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Creator } from './creator';
 import { Setting } from './setting';
 
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 import { Provider } from 'src/context/provider';
 import { Nav } from './nav';
 import { Customer } from './customer';
 import { ItemManager } from './item';
 
+const LoadResource = (props: { children: any }) => {
+  const [finish, setFinish] = React.useState(false);
+  React.useEffect(() => {
+    const jobs = [
+      import('@blueprintjs/core/lib/css/blueprint.css'),
+      import('@blueprintjs/datetime/lib/css/blueprint-datetime.css')
+    ];
+    Promise.all(jobs).then(() => setFinish(true));
+  }, []);
+  return finish ? (
+    props.children
+  ) : (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      Loading ...
+    </div>
+  );
+};
 const AppComponent = () => {
   return (
     <div>
-      <Provider>
-        <Nav />
-        <div style={{ padding: 10 }}>
-          <Router>
-            <Switch>
-              <Route path="/setting">
-                <Setting />
-              </Route>
-              <Route path="/customer">
-                <Customer />
-              </Route>
-              <Route path="/item">
-                <ItemManager />
-              </Route>
-              <Route path="/">
-                <Creator />
-              </Route>
-            </Switch>
-          </Router>
-        </div>
-      </Provider>
+      <LoadResource>
+        <Provider>
+          <Nav />
+          <div style={{ padding: 10 }}>
+            <Router>
+              <Switch>
+                <Route path="/setting">
+                  <Setting />
+                </Route>
+                <Route path="/customer">
+                  <Customer />
+                </Route>
+                <Route path="/item">
+                  <ItemManager />
+                </Route>
+                <Route path="/">
+                  <Creator />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        </Provider>
+      </LoadResource>
     </div>
   );
 };
